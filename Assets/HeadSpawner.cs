43439170749cc2f1f4e3f5df701312e24ddc9a8a
Diego N <54142512+dnmaster2@users.noreply.github.com>
2020-05-17 20:31:36 -0300

    Cena dos créditos com as cabeças bizarras pronta

---
+++ Assets/HeadSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadSpawner : MonoBehaviour
+{
+    public List<GameObject> Pessoas;
+    public Transform spawnPoint;
+
+    private void OnMouseEnter()
+    {
+        foreach(GameObject pessoa in Pessoas)
+        {
+            Instantiate(pessoa, spawnPoint.position, Quaternion.identity, transform);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+    }
+}
