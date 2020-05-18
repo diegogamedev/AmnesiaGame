21fb874341c0769ecdd1eb62d223363cbdeb8727
Diego N <54142512+dnmaster2@users.noreply.github.com>
2020-05-18 00:15:21 -0300

    Botão de quit oporra kkk

--- Assets/HeadSpawner.cs
+++ Assets/HeadSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HeadSpawner : MonoBehaviour
 {
@@ -29,5 +30,10 @@ public class HeadSpawner : MonoBehaviour
         {
             Destroy(transform.GetChild(i).gameObject);
         }
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
